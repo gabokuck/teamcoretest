edbcf30c25e87e469f24e8bbd9324779fd1d42e9
Gabriel Rosa <gabokuck1990@gmail.com>
2024-06-19 15:51:03 -0600

    configuraciones iniciales

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import {TeamcoreApp} from './src/TeamcoreApp';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => TeamcoreApp);
---
+++ src/TeamcoreApp.tsx
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+export const TeamcoreApp = () => {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>TeamcoreApp</Text>
+      </View>
+    </NavigationContainer>
+  );
+};
